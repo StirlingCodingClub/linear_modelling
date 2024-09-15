c9c3f08de01a0c7d45e0197cad9eb1fe03f5e1b4
Brad Duthie <brad.duthie@gmail.com>
2024-09-16 00:24:34 +0100

    Update the notes

--- scratch.R
+++ scratch.R
@@ -1,4 +1,3 @@
-
 plant_data <- read.csv(file = "two_discrete_x_values.csv");
 
 species_1 <- plant_data$height[plant_data$species_ID == "species_1"];
@@ -7,59 +6,38 @@ hist(species_1, breaks = 10, col = "blue", xlim = c(0, 300),
      main = "", xlab = "Plant height", cex.lab = 1.25, 
      ylim = c(0, 15));
 hist(species_2, breaks = 10, col = "red", add = TRUE)
-legend(x = 0, y = 15, fill = c("red", "blue"), 
-       legend = c("species_2", "species_1"));
+#legend(x = 0, y = 15, fill = c("red", "blue"), 
+#       legend = c("species_2", "species_1"));
 
 
+n1     <- length(species_1);
+n2     <- length(species_2);
+var_y1 <- var(species_1);
+var_y2 <- var(species_2);
+s_p    <- sqrt(((n1 - 1)*var_y1 + (n2 - 1)*var_y2)/(n1 + n2 - 2));
+SEybar <- s_p * sqrt((n1 + n2) / (n1*n2));
+tval   <- (mean(species_1) - mean(species_2)) / SEybar;
 
-
-
-#################
 #################
 #################  Using the t-test function in R and lm
 #################
-#################
-t.test(species_1, species_2, var.equal = TRUE);
+t.test(species_1, species_2, var.equal = FALSE);
+
+# Use a linear model instead to get the same numbers
 lmod1 <- lm(height ~ 1 + species_ID, data = plant_data);
-lmod1 <- lm(height ~ species_ID, data = plant_data);
 summary(lmod1);
 
 
-
-
-
-
-
-
-
-#################
 #################
 #################  How R sees the linear model and t-test
-#################
 #################
 is_species_2 <- as.numeric(plant_data$species_ID == "species_2");
 plant_height <- plant_data$height;
 lm_table_eg  <- data.frame(plant_height, is_species_2);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#################
 #################
 #################  Thinking about this in a plot
-#################
 #################
 plot(x = lm_table_eg$is_species_2, y = lm_table_eg$plant_height, 
      ylim = c(0, 275), pch = 20, cex.axis = 1.25, cex.lab = 1.25, 
@@ -75,49 +53,15 @@ points(x = 1, y = summary(lmod1)$coefficients[1,1] +
          summary(lmod1)$coefficients[2,1], pch = 18, col = "orange", cex = 3);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#################
 #################
 #################  Analysis of variance with two categories
-#################
 #################
 aov_1 <- aov(height ~ species_ID, data = plant_data);
 summary(aov_1);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#################
 #################
 #################  What happens when we have three categories? ANOVA
-#################
 #################
 plant_data <- read.csv(file = "three_discrete_x_values.csv");
 aov_2 <- aov(height ~ species_ID, data = plant_data);
@@ -125,23 +69,8 @@ summary(aov_2);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#################
 #################
 #################  Three categories with a linear model
-#################
 #################
 lmod2 <- lm(height ~ 1 + species_ID, data = plant_data);
 summary(lmod2);
@@ -151,29 +80,8 @@ tapply(X = plant_data$height, INDEX = plant_data$species_ID,
        FUN = mean);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#################
 #################
 #################  What's really going on with 3 groups?
-#################
 #################
 the_intercept <- rep(x = 1, length = length(plant_data$height));
 is_species_2  <- as.numeric(plant_data$species_ID == "species_2");
@@ -188,23 +96,8 @@ head(lm_table_eg);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#################
 #################
 #################  Even more elegant, using matrices!
-#################
 #################
 Y <- as.matrix(lm_table_eg[,1]);
 X <- as.matrix(lm_table_eg[,2:4]);
